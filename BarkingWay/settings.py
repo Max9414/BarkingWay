7ceb8d6d634ca8eebd46f1a7cbce68b416c298c6
Max94 <maxy_14@hotmail.it>
2024-06-03 18:55:54 +0000

    Moved the secret key

--- BarkingWay/settings.py
+++ BarkingWay/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%(mg0zb+0@s28e+ys=z5(@kk6*+$d&spq2ivuwo#f6gzy^%ha*'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
