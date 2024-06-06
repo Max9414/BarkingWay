4886b53743d37e36d715a9d36b6ed7162b9250ff
Max94 <maxy_14@hotmail.it>
2024-06-06 17:16:38 +0000

    Added models in events and fixed bottom padding

--- events/models.py
+++ events/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Location(models.Model):
+    location = models.CharField(max_length=200)
+
+class Event(models.Model):
+    event = models.CharField(max_length=200)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_event')
+    updated_on = models.DateTimeField(auto_now_add=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, name="location")
+    description = models.TextField()
+    participants = models.IntegerField(blank=True)
